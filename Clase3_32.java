6c3cdadab2de0d91b48ffc7cdcc61b0d9e23fab5
Flashut <pndcr26@gmail.com>
2024-10-24 16:07:29 +0200

    Update Clase3_32.java

--- Clase3_32.java
+++ Clase3_32.java
@@ -4,9 +4,9 @@ public class Clase3_32 {
 
 	public static void main(String[] args) {
 
-		for (int filas = 0; filas < 5; filas++) {
+		for (int filas = 0; filas < 6; filas++) {
 
-			for (int columnas = 0; columnas < 5; columnas++) {
+			for (int columnas = 0; columnas < 6; columnas++) {
 
 				if (columnas == filas) {
 					System.out.print("*");
