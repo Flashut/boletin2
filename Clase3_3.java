1934a03911dcbde98292de46302e0b0733cf942a
Flashut <pndcr26@gmail.com>
2024-10-24 16:04:35 +0200

    Update Clase3_3.java

--- Clase3_3.java
+++ Clase3_3.java
@@ -3,7 +3,7 @@ package boletin2;
 public class Clase3_3 {
 
 	public static void main(String[] args) {
-		System.out.println("Clase3_3 Juan David Cardenas");
+		System.out.println("Canela En Rama");
 
 	}
 
